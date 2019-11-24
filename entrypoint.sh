e8c97d185d0cb6f0ee119ebed329432d9d940a18
Nick DeRobertis <whoopnip@gmail.com>
2019-11-24 14:55:56 -0500

    Add error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
 
 version=$(python get_version.py $INPUT_PACKAGE)
+
+if [ $? -ne 0 ]; then
+    echo "Error getting package version from PyPI" >&2;
+
+    exit 1;
+fi
+
 echo ::set-output name=version::$version
